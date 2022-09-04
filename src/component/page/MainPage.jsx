d0352de25394384be29dcd6ce3ed05bc03dad9ed
Soohyeun <soohyeunp@gmail.com>
2022-09-04 01:16:56 -0700

    Create draft landing page

--- src/component/page/MainPage.jsx
+++ src/component/page/MainPage.jsx
@@ -1,74 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
+import axios from "axios";
 import PokeCard from "../ui/PokeCard";
 
 const Wrapper = styled.div`
   display: flex;
-  flex-direction: row;
+  flex-flow: row wrap;
   justify-content: space-around;
+  perspective: 1000px;
 `;
 
-const ContainerRow = styled.div`
-  display: flex;
-  flex-direction: column;
-  width: 25%;
-`;
+// const ContainerRow = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   width: 25%;
+// `;
 
 function MainPage(props) {
-    return (
-        <Wrapper>
-            <ContainerRow>
-                <PokeCard
-                    name="ferroseed"
-                    image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/228.png"
-                    type="grass"
-                />
-                <PokeCard
-                    name="ferroseed"
-                    image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/228.png"
-                    type="grass"
-                />
-                <PokeCard
-                    name="ferroseed"
-                    image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/228.png"
-                    type="grass"
-                />
-            </ContainerRow>
-            <ContainerRow>
-                <PokeCard
-                    name="ferroseed"
-                    image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/228.png"
-                    type="grass"
-                />
-                <PokeCard
-                    name="ferroseed"
-                    image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/228.png"
-                    type="grass"
-                />
-                <PokeCard
-                    name="ferroseed"
-                    image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/228.png"
-                    type="grass"
-                />
-            </ContainerRow>
-            <ContainerRow>
-                <PokeCard
-                    name="ferroseed"
-                    image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/228.png"
-                    type="grass"
-                />
-                <PokeCard
-                    name="ferroseed"
-                    image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/228.png"
-                    type="grass"
-                />
-                <PokeCard
-                    name="ferroseed"
-                    image="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/228.png"
-                    type="grass"
-                />
-            </ContainerRow>
-        </Wrapper>
-    );
+  const [pokeInfo, setPokeInfo] = useState([]);
+
+  async function getPokeInfo() {
+    const temp = [];
+    for (let i = 0; i < 9; i++) {
+      let random_number = Math.floor(Math.random() * 800) + 1;
+      await axios
+        .get(`https://pokeapi.co/api/v2/pokemon/${random_number}`)
+        .then((response) => {
+          temp.push(response.data);
+        });
+    }
+    setPokeInfo(temp);
+  }
+
+  useEffect(() => {
+    getPokeInfo();
+  }, []);
+
+  console.log(pokeInfo);
+
+  return (
+    <Wrapper>
+      {pokeInfo.map((data, index) => {
+        return (
+          <PokeCard
+            key={index}
+            name={data.name}
+            image={data.sprites.other["official-artwork"].front_default}
+            type={data.types[0].type.name}
+          />
+        );
+      })}
+    </Wrapper>
+  );
 }
 export default MainPage;
--- src/component/ui/PokeCard.jsx
+++ src/component/ui/PokeCard.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import styled from "styled-components";
+import ReactCardFlip from "react-card-flip";
+import { useState } from "react";
 
 const StyledCard = styled.div`
   border: #333 solid 1.5px;
@@ -7,20 +9,61 @@ const StyledCard = styled.div`
   padding: 30px 0px 20px 0px;
   margin: 30px 0;
   text-align: center;
+  width: 28%;
+`;
+
+const StyledFront = styled.div`
+  width: 100%;
+  height: 100%;
+`;
+
+const StyledBack = styled.div`
+  width: 100%;
+  height: 100%;
 `;
 
 const typeColour = {
-    "grass" : "#DEFDE0",
+  fire: "#FDDFDF",
+  grass: "#DEFDE0",
+  electric: "#FCF7DE",
+  water: "#DEF3FD",
+  ground: "#f4e7da",
+  rock: "#d5d5d4",
+  fairy: "#fceaff",
+  poison: "#98d7a5",
+  bug: "#f8d5a3",
+  dragon: "#97b3e6",
+  psychic: "#eceda1",
+  flying: "#F5F5F5",
+  fighting: "#E6E0D4",
+  normal: "#F5F5F5",
+  dark: "#A9A9A9",
+  ghost: "#DCDCDC",
+  ice: "#F5FFFA",
+  steel: "#708090",
 };
 
 function PokeCard(props) {
-    const {name, image, type} = props;
+  const { name, image, type } = props;
+  const [flip, setFlip] = useState(false);
 
-    return (
-    <StyledCard style={{backgroundColor: typeColour[type]}}>
-        <img src={image} alt={name} style={{width: "100%"}}/>
-        <h4>{name}</h4>
-    </StyledCard>)
+  return (
+    <StyledCard
+      style={{ backgroundColor: typeColour[type] }}
+      onClick={() => {setFlip(!flip)}}
+    >
+      <ReactCardFlip isFlipped={flip} flipDirection="horizontal">
+        <StyledFront>
+          <img src={image} alt={name} style={{ width: "100%" }} />
+          <h4>{name}</h4>
+        </StyledFront>
+
+        <StyledBack>
+          <h4>{name}</h4>
+        </StyledBack>
+      </ReactCardFlip>
+    </StyledCard>
+  );
 }
 
 export default PokeCard;
